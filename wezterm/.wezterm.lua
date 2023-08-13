f5781785750b62d49c5bfdb081b2022239936ecd
Jered <jeredlittle1996@gmail.com>
2023-08-13 09:20:26 -0600

    added wezterm

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,45 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+-- This table will hold the configuration.
+local config = {}
+local act = wezterm.action
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+-- This is where you actually apply your config choices
+-- For example, changing the color scheme:
+config.color_scheme = 'GruvboxDarkHard'
+config.window_decorations = "RESIZE"
+config.enable_tab_bar = false
+config.font = wezterm.font 'JetBrains Mono'
+config.keys = {
+  { key = "l", mods = "CTRL", action = wezterm.action.ShowLauncher },
+  { key = '{', mods = 'ALT',  action = act.ActivateTabRelative(-1) },
+  { key = '}', mods = 'ALT',  action = act.ActivateTabRelative(1) },
+
+}
+config.window_background_opacity = .9
+-- Default program
+config.default_prog = { "powershell.exe", "ubuntu", "-c", "zsh" }
+config.launch_menu = {
+  {
+    label = "Windows: WSL (Ubuntu ZSH)",
+    args = { "powershell.exe", "ubuntu", "-c", "zsh" }
+  },
+  {
+    label = "Windows: Dev Container (Archlinux)",
+    args = { "ubuntu", "-c", "zsh", "cd $HOME/dev-containers && make run" }
+  },
+  {
+    label = "OSX: ZSH",
+    args = { "/bin/zsh" }
+  },
+  {
+    label = "OSX: Dev Container (Archlinux)",
+    args = { "/bin/zsh", "cd $HOME/dev-containers/ && make run" }
+  }
+}
+return config
