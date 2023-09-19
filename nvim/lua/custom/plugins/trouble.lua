4dbd624645cc43a2ddca937a77e13ccd41f9716c
Jered <jeredlittle1996@gmail.com>
2023-09-19 04:08:34 -0600

    add trouble keybind

--- nvim/lua/custom/plugins/trouble.lua
+++ nvim/lua/custom/plugins/trouble.lua
@@ -3,6 +3,10 @@ return {
     "folke/trouble.nvim",
     opts = {
     },
+    config = function()
+      vim.keymap.set("n", "<leader>t", "<cmd>TroubleToggle<cr>",
+        { desc = "[T]rouble Toggle", noremap = true, silent = true })
+    end,
     lazy = false
   },
 }
