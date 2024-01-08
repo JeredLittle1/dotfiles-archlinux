cb7b8a9cb0f361ff4d2991eddb6d5b061990f5aa
Jered Little <jeredlittle1996@gmail.com>
2024-01-08 07:40:04 -0700

    copilot remap

--- nvim/lua/custom/plugins/copilot.lua
+++ nvim/lua/custom/plugins/copilot.lua
@@ -1,6 +1,10 @@
 return {
   {
     'github/copilot.vim',
-    lazy = false
+    lazy = false,
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    end
   }
 }
